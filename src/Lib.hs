39bda8c74c83c4ff8b0aa42aabed96e92516b9bd
Bjorn Tipling <bjorn@ambientchill.com>
2022-12-19 06:23:07 +0000

    Pick a random word

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Lib
     ( someFunc
     ) where
 
+
+import Data.ByteString
+import System.Random
+import qualified Data.List
+import Data.FileEmbed
+import Data.Text.Encoding
+import Data.Text
+
+contentBytes :: Data.ByteString.ByteString
+contentBytes = $(embedFile "./resources/words.txt")
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do 
+    putStrLn "Hello!"
+    let contentsText = decodeUtf8 contentBytes
+    let contents = Data.Text.unpack contentsText
+    let gameWords = Data.List.lines contents
+    randI <- randomRIO (0, Data.List.length gameWords - 1)
+    -- print a random word
+    putStrLn (gameWords !! randI)
